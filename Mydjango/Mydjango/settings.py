cb59009cac0c3c06df670c0e9d011736f9e2b186
Umar-silicon <umar@sphericesolutions.com>
2025-09-05 20:19:01 +0500

    add admin and taiwind setup

--- Mydjango/Mydjango/settings.py
+++ Mydjango/Mydjango/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "authSys",
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = "theme"
+INTERNAL_IPS=['127.0.0.1']
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
